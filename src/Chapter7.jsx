5136575efc8487e100044f9c4af02cef9b2f9c92
akmalputrautomo <apu.utomo@gmail.com>
2023-11-17 20:06:40 +0700

    first commit

---
+++ src/Chapter7.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Chapter7 = () => {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-black text-8xl">AKMAL PUTRA UTOMO</h1>
+    </div>
+  );
+};
